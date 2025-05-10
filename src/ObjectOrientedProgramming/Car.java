7fe014b5dfdd93440306810f727d1c9cc2a97530
Rachit0910d <rachit.rrr2005@gmail.com>
2025-05-10 21:13:41 +0530

    JAVA-course

---
+++ src/ObjectOrientedProgramming/Car.java
@@ -0,0 +1,8 @@
+package ObjectOrientedProgramming;
+
+public class Car {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/ObjectOrientedProgramming/Driver.java
@@ -0,0 +1,7 @@
+package ObjectOrientedProgramming;
+
+import java.util.Scanner;
+
+public class Driver {
+    
+}
